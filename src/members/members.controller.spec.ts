4828261c1a02f6fcfe33991b48a152cde1fef15a
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2023-06-26 04:58:45 +0900

    up

--- src/members/members.controller.spec.ts
+++ src/members/members.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MembersController } from './members.controller';
+import { MembersService } from './members.service';
 
 describe('MembersController', () => {
   let controller: MembersController;
@@ -7,6 +8,7 @@ describe('MembersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MembersController],
+      providers: [MembersService],
     }).compile();
 
     controller = module.get<MembersController>(MembersController);
