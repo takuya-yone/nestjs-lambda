42b7b203c2a219438f66e92d8ed1d90f492c67a7
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2023-07-17 17:01:12 +0900

    up

--- prisma/ramdom.ts
+++
@@ -1,9 +0,0 @@
-import { randEmail, randFullName } from '@ngneat/falso';
-
-// const user = { email: randEmail(), name: randFullName() };
-
-const users: string[] = randFullName({ length: 10 });
-const emails: string[] = randEmail({ length: 10 });
-
-console.log(users);
-console.log(emails);
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,44 +1,53 @@
+interface PostRecord {
+  title: string;
+  content: string;
+  published: boolean;
+}
+
+import {
+  randEmail,
+  randFullName,
+  rand,
+  seed,
+  randSuperhero,
+  randMovie,
+  randUrl,
+  randBoolean,
+} from '@ngneat/falso';
+
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
+
+const users: string[] = randFullName({ length: 20 });
+
 async function main() {
-  const alice = await prisma.user.upsert({
-    where: { email: 'alice@prisma.io' },
-    update: {},
-    create: {
-      email: 'alice@prisma.io',
-      name: 'Alice',
-      posts: {
-        create: {
-          title: 'Check out Prisma with Next.js',
-          content: 'https://www.prisma.io/nextjs',
-          published: true,
+  for (const user of users) {
+    const email: string = randEmail();
+    const x: number = Math.floor(Math.random() * 9) + 1;
+    const posts: PostRecord[] = [];
+
+    for (let i = 0; i < x; i++) {
+      const tmp = {
+        title: randMovie(),
+        content: randUrl(),
+        published: randBoolean(),
+      };
+      posts.push(tmp);
+    }
+
+    const record = await prisma.user.upsert({
+      where: { email: email },
+      update: {},
+      create: {
+        email: email,
+        name: user,
+        posts: {
+          create: posts,
         },
       },
-    },
-  });
-  const bob = await prisma.user.upsert({
-    where: { email: 'bob@prisma.io' },
-    update: {},
-    create: {
-      email: 'bob@prisma.io',
-      name: 'Bob',
-      posts: {
-        create: [
-          {
-            title: 'Follow Prisma on Twitter',
-            content: 'https://twitter.com/prisma',
-            published: true,
-          },
-          {
-            title: 'Follow Nexus on Twitter',
-            content: 'https://twitter.com/nexusgql',
-            published: true,
-          },
-        ],
-      },
-    },
-  });
-  console.log({ alice, bob });
+    });
+    console.log(record);
+  }
 }
 main()
   .then(async () => {
